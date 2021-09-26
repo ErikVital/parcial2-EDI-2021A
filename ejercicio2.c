afd9856d8c9d1e67e2ac8d8538625e68ebecbb31
ErikVital <89081103+ErikVital@users.noreply.github.com>
2021-09-26 01:57:21 -0500

    Update ejercicio2.c

--- ejercicio2.c
+++ ejercicio2.c
@@ -1,1 +1,33 @@
+//Erik Vital Muñoz
 
+//Git no quiere nomas en mi compu, una disculpa y estoy tratando de subirlo en todas partes y que pueda ver mis tareas :c
+#include <stdio.h>
+
+#define N 5
+
+int main()
+{
+	int matriz[N][N];
+	int i, j;
+	
+	for(i=0 ; i < N ; i++)
+	{
+		for(j=0; j < N ; j++)
+		{
+			if(i==j)
+			{
+				matriz[j][i]=1;
+				//printf("%d", matriz[j][i]);
+			}
+			else
+			{
+				matriz[j][i]=0;
+				//printf("%d", matriz[j][i]);
+			}
+			printf("%d", matriz[j][i]);
+		}
+		printf("\n");
+	}
+	
+	return 0;
+}
